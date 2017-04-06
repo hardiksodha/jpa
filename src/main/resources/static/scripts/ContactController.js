42f7ea5787f39780bda5cc80f31cdbf6d3b6174b
Hardik <hardikbsodha@gmail.com>
2017-04-06 21:56:37 +0530

    removing unused ui files for jpa

--- src/main/resources/static/scripts/ContactController.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Hardik on 08-03-2017.
- */
-
-myApp.controller('contactController', function ($scope) {
-
-    $scope.myText = "I am in contactController controller";
-
-})
--- src/main/resources/static/scripts/MainController.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Hardik on 08-03-2017.
- */
-
-myApp.controller('mainController', function ($scope) {
-
-    $scope.myText = "I am in main controller";
-
-})
